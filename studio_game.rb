14bfd1e8fa5ef312cdb4b2c3df755a9d3c4b1a2c
Waihon Yew <yewwaihon@gmail.com>
2016-09-07 18:38:08 +0800

    Complete 4. Variables and Objects

--- studio_game.rb
+++ studio_game.rb
@@ -1,10 +1,35 @@
+# Player 1: Larry
 name1 = "larry"
-
 health1 = 60
+puts "#{name1.capitalize} has a health of #{health1}."
 
-puts "#{name1}'s health is #{health1 / 9.0}"
-puts "#{name1}'s health is #{health1 / 9}"
-
+# Player 2: Curly
 name2 = "curly"
+health2 = 125
+puts "#{name2.upcase} has a health of #{health2}."
+
+health2 = health1
+puts "#{name2.upcase} has a health of #{health2}."
+
+health1 = 30
+puts "#{name1.capitalize} has a health of #{health1}."
+puts "#{name2.upcase} has a health of #{health2}."
+
+# Player #3: Moe
 name3 = "moe"
-puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
+health3 = 100
+puts "#{name3.capitalize} has a health of #{health3}.".center(50, "*")
+
+# Player #4: Shemp
+name4 = "shemp"
+health4 = 90
+puts "#{name4.capitalize.ljust(30, ".")} #{health4} health"
+
+# Reverse It
+number = 123
+reversed_number = number.to_s.reverse.to_i
+puts reversed_number
+
+# Time Method
+current_time = Time.new
+puts "The game started on #{current_time.strftime("%A %m/%d/%Y at %I:%M%p")}"
