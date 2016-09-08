18509746471be9b54362153b8b88729660725f8e
Waihon Yew <yewwaihon@gmail.com>
2016-09-08 20:59:58 +0800

    Complete 8. Attributes

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,18 @@
 class Player
+  attr_accessor :name  
+  attr_reader :health
+
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
 
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
+
   def to_s
-    "I'm #{@name} with a health of #{@health}."
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 
   def blam
@@ -17,11 +24,14 @@ class Player
     @health += 15
     puts "#{@name} got w00ted!"
   end
+
+  def score
+    @health + @name.length
+  end
 end
 
 player1 = Player.new("moe")
 puts player1.inspect
-puts player1
 puts player1
 player1.blam
 puts player1
@@ -30,6 +40,8 @@ player2 = Player.new("larry", 60)
 puts player2
 player2.w00t
 puts player2
+player2.name = "lawrance"
+puts player2.name
 
 player3 = Player.new("curly", 125)
 puts player3
@@ -37,3 +49,7 @@ player3.blam
 puts player3
 player3.w00t
 puts player3
+
+puts player1.health
+puts player2.health
+puts player3.health
