63fe91f52db83b0fa0f773643138bf7d6db8e098
Waihon Yew <yewwaihon@gmail.com>
2016-09-14 20:26:33 +0800

    Complete 14. Modules

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'die'
+require_relative 'game_turn'
 
 class Game
   attr_reader :title
@@ -20,15 +21,7 @@ class Game
     end
 
     @players.each do |player|
-      die = Die.new
-      case die.roll
-      when 1..2
-        player.blam
-      when 3..4
-        puts "#{player.name} was skipped."
-      else
-        player.w00t
-      end
+      GameTurn.take_turn(player)      
       puts player
     end
   end
