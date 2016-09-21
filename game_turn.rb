92802fbf8ead1bd05634ba610af5a346bdd1bd48
Waihon Yew <yewwaihon@gmail.com>
2016-09-21 09:32:01 +0800

    Complete 16. Symbols and Structs

--- game_turn.rb
+++ game_turn.rb
@@ -1,5 +1,6 @@
 require_relative 'die'
 require_relative 'player'
+require_relative 'treasure_trove'
 
 module GameTurn
   def self.take_turn(player)
@@ -13,5 +14,8 @@ module GameTurn
     else
       player.w00t
     end    
+
+    treasure = TreasureTrove.random
+    puts "#{player.name} found a #{treasure.name} worth #{treasure.points} points."
   end
 end
